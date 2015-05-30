7e45caac0d3db619a96cf219f756b03d6cd965e1
Shaula <the9ball@gmail.com>
2015-05-30 15:02:11 +0900

    [Disposable] create IDisposable variation

--- Disposable.php
+++ Disposable.php
@@ -5,3 +5,18 @@ interface IDisposable
 	public function Dispose();
 }
 
+class EmptyDisposable implements IDisposable
+{
+	public function Dispose(){}
+}
+
+class SingleAssignmentDisposable implements IDisposable
+{
+	public $disposable;
+
+	public function Dispose()
+	{
+		if ( null != $this->disposable ) $this->disposable->Dispose();
+	}
+}
+
